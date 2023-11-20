fe47c07f5e1a538ed91f8898168d0131e1527e15
Chathura Jayawardhana <105495683+ChathurJayawardhana@users.noreply.github.com>
2023-11-20 10:50:40 +0530

    complete signup ui

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -23,10 +23,10 @@ export default function Signup() {
         disabled:opacity-80'
         >Sign Up</button>
       </form>
-      <div>
+      <div className='flex gap-2 mt-5' >
         <p>Have an account ?</p>
         <Link to={"/sign-in"}>
-        <span>Sign In</span>
+        <span className='text-blue-700'>Sign In</span>
         </Link>
       </div>
     </div>
